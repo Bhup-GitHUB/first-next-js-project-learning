52a13caab10690203f11968abc1ac92eaf70a2c0
Bhupesh Kumar <bkumar_be23@thapar.edu>
2024-10-31 15:58:05 +0530

    routes learning

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       <h1 className="h1-bold"> Next.js 13 is a new version of Next.js.</h1>
       <h2 className="h2-bold">Next.js 13 is a new version of Next.js.</h2>
       <h3 className="h3-bold">Next.js 13 is a new version of Next.js.</h3>
+      <h4 className="h3-semibold">Next.js 13 is a new version of Next.js.</h4>
     </div>
   );
 }
