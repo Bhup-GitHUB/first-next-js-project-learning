e6a44e897de3b77b9278515c471661428c2ac7d1
Bhupesh Kumar <bkumar_be23@thapar.edu>
2024-10-31 16:35:53 +0530

    learning routes in nextjs

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      sign up
+    </div>
+  );
+};
+
+export default SignUp;
